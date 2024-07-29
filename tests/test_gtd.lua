08ef65b9880135c6c328b959fe118870da976865
Timothy Crundall <tim.crundall@gmail.com>
2024-07-29 21:57:35 +0200

    feat(unit tests): add for is_action

--- tests/test_gtd.lua
+++ tests/test_gtd.lua
@@ -1,15 +1,32 @@
+-- Define helper aliases
 local new_set = MiniTest.new_set
 local expect, eq = MiniTest.expect, MiniTest.expect.equality
 
-local T = new_set()
+-- Create (but not start) child Neovim object
+local child = MiniTest.new_child_neovim()
 
--- Actual tests definitions will go here
-T["works"] = function()
-    local x = 1 + 1
-    if x ~= 2 then
-        error("`x` is not equal to 2")
-    end
-    eq(x, 2)
+-- Define main test set of this file
+local T = new_set({
+    -- Register hooks
+    hooks = {
+        -- This will be executed before every (even nested) case
+        pre_case = function()
+            -- Restart child process with custom 'init.lua' script
+            child.restart({ "-u", "scripts/minimal_init.lua" })
+            -- Load tested plugin
+            child.lua([[M = require('gtd')]])
+        end,
+        -- This will be executed one after all tests from this set are finished
+        post_once = child.stop,
+    },
+})
+
+T["is_action is correct"] = function()
+    eq(child.lua_get([[M.is_action("- [ ] action")]]), true)
+    eq(child.lua_get([[M.is_action("- [ ] ")]]), true)
+    eq(child.lua_get([[M.is_action("- [ ]")]]), false)
+    eq(child.lua_get([[M.is_action("- [x] action")]]), false)
+    eq(child.lua_get([[M.is_action("")]]), false)
 end
 
 return T
