dacf0b4e470c5de094708033f0750e4455af7bfa
Timothy Crundall <tim.crundall@gmail.com>
2024-09-07 11:08:29 +0200

    feat(target): Expose targetting as user function

--- lua/gtd/init.lua
+++ lua/gtd/init.lua
@@ -7,6 +7,7 @@ package.loaded["gtd.random_tags"] = nil
 
 local checkboxes = require("gtd.checkboxes")
 local sync = require("gtd.sync")
+local target = require("gtd.target")
 
 local M = {}
 
@@ -14,10 +15,15 @@ M.setup = function() end
 
 M.cycle_checkbox = checkboxes.cycle_checkbox
 M.scrape_actions = sync.scrape_actions
+M.target_action = target.target_action
 
 vim.api.nvim_create_user_command("CycleCheckbox", function(opts)
     local bool_val = opts.fargs[1] == "true"
     M.cycle_checkbox(bool_val)
 end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("GtdTargetAction", function()
+    M.target_action()
+end, {})
 
 return M
